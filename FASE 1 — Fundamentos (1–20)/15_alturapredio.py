b8d79137d9d32c73c0416d31a0728076eb136391
Christie <gabriellechristie000@gmail.com>
2026-05-25 18:04:47 -0300

    Resolução exercício 15

---
+++ FASE 1 — Fundamentos (1–20)/15_alturapredio.py
@@ -0,0 +1,13 @@
+''' 15. Num dia de sol, você deseja medir a altura de um prédio, porém, a trena não é
+suficientemente longa. Assumindo que seja possível medir sua sombra e a do prédio no
+chão, e que você lembre da sua altura, faça um programa para ler os dados necessários e
+calcular a altura do prédio.'''
+
+
+sombra_predio = float(input("Digite o tamanho da sombra do prédio em metros: "))
+minha_altura = float(input("Digite a sua altura em metros: "))
+minha_sombra = float(input("Digite o tamanho da sua sombra em metros: "))
+
+altura_predio = (minha_altura * sombra_predio) / minha_sombra
+
+print(f"A altura do prédio é {altura_predio:.2f} metros")
