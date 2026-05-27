8ab6bc45087428a1ca5e51543c04f5905ba0647b
Christie <gabriellechristie000@gmail.com>
2026-05-27 17:16:31 -0300

    Resolução exercicio 18

---
+++ FASE 1 — Fundamentos (1–20)/18_equaçao2grau.py
@@ -0,0 +1,17 @@
+'''Construa um programa para calcular as raizes de uma equação do segundo grau(ax^2 + bx + c = 0), sendo que o valores a,b,c são fornecidos pelo usário(considere que a equação possui duas raizes reais).'''
+
+import math
+
+a = float(input("Digite o valor de a:"))
+b = float(input("Digite o valor de b:"))
+c = float(input("Digite o valor de c:"))
+
+delta = b**2 - 4*a*c
+if delta < 0:
+    print("A equação não possui raízes reais.")
+    exit()
+else:
+    raiz1 = (-b + math.sqrt(delta))/(2*a)
+    raiz2 = (-b - math.sqrt(delta))/(2*a)
+
+print(f"As raízes da equação são: {raiz1} e {raiz2}")
