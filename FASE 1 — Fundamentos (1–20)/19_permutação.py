e972be44300adb2524cd04b4837c7a580831def2
Christie <gabriellechristie000@gmail.com>
2026-05-27 17:25:25 -0300

    Resolução exercicio 19

---
+++ FASE 1 — Fundamentos (1–20)/19_permutação.py
@@ -0,0 +1,10 @@
+'''Escreva um programa que permute o valor de duas variáveis inteiras'''
+
+a = int(input("Digite o valor de a:"))
+b = int(input("Digite o valor  de b:"))
+
+permutacao_a = b 
+permutacao_b = a
+
+print(f"O valor de a é: {permutacao_a}")
+print(f"O valor de b é: {permutacao_b}")
