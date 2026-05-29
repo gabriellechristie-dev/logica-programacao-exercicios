4c76258a2c4a25690f1712fdf2a48af924b3eb9c
Christie <gabriellechristie000@gmail.com>
2026-05-29 17:41:15 -0300

    resolução exercicio 23

---
+++ FASE 1 — Fundamentos (1–20)/23_valormercadoria.py
@@ -0,0 +1,19 @@
+"""
+Uma loja vende seus produtos no sistema entrada mais duas prestações, sendo a entrada
+maior do que ou igual às duas prestações, as quais devem ser iguais, inteiras e as maiores
+possíveis. Por exemplo, se o valor da mercadoria for R$ 270,00, a entrada e as duas
+prestações são iguais a R$ 90,00; se o valor da mercadoria for R$ 302,75, a entrada é de
+R$ 102,75 e as duas prestações são a iguais a R$ 100,00. Escreva um programa que
+receba o valor da mercadoria e forneça o valor da entrada e das duas prestações, de
+acordo com as regras acima. Observe que uma justificativa para a adoção desta regra é
+que ela facilita a confecção e o consequente pagamento dos boletos das duas prestações.
+"""
+
+valor_mercadoria = float(input("Digite o valor da mercadoria: R$ "))
+
+prestacao = valor_mercadoria // 3
+entrada = valor_mercadoria - prestacao * 2
+
+
+print(f"Valor da entrada: R$ {entrada:.2f}")
+print(f"Valor de cada prestação: R$ {prestacao:.2f}")
