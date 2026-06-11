d3837581a866bde15156526689bf529f6d919a89
Christie <gabriellechristie000@gmail.com>
2026-06-11 18:13:55 -0300

    .

---
+++ FASE 1 — Fundamentos (1–20)/estudante.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que apresente um estudante universitário considerando seu nome, curso que estuda, semestre atual e seu hobby favorito.
+
+"""
+
+nome = input("Digite o nome do estudante:")
+curso = input("Digite o curso que o estudante cursa:")
+semestre = input("Digite o semestre atual do estudante (ex:primeiro):")
+hobby = input("Digite o hobby favorito do estudante:")
+
+print(f"Prazer, eu sou o(a) {nome}!")
+print(f"Atualmente, estou no {semestre} semestre de {curso} e meu hobby favorito é {hobby}.")
